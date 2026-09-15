f327da1717f5db5dc4bf72f7893d6ba86ebe6938
fnig2rm632 <fnig2rm632@yandex.ru>
2026-09-15 18:01:59 +0300

    feat(infrastructure): registration repositories in di

--- src/Remby.Infrastructure/DependencyInjection.cs
+++ src/Remby.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Npgsql;
+using Remby.Application.Interfaces.Command;
+using Remby.Application.Interfaces.Query;
 using Remby.Infrastructure.Data;
+using Remby.Infrastructure.Persistence.Command;
+using Remby.Infrastructure.Persistence.Query;
+using Npgsql;
 
 namespace Remby.Infrastructure;
 
@@ -9,12 +13,19 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddSingleton<NpgsqlConnection>(_ => 
+        services.AddScoped<NpgsqlConnection>(_ => 
             new NpgsqlConnection(configuration.GetConnectionString("DefaultConnection"))
         );
-        
+
+        services.AddScoped<IUserQueryRepository, UserQueryRepository>();
+        services.AddScoped<IFolderQueryRepository, FolderQueryRepository>();
+        services.AddScoped<ICardQueryRepository, CardQueryRepositor>();
+        services.AddScoped<IUserCommandRepository, UserCommandRepository>();
+        services.AddScoped<IFolderCommandRepository, FolderCommandRepository>();
+        services.AddScoped<ICardCommandRepository, CardCommandRepository>();
+
         DatabaseInitializer.Initializer(configuration);
-        
+
         return services;
     }
 }
